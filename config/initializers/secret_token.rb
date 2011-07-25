8e3d16366085d2792fe30622b2cb18c7f0ec2f0b
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2011-07-25 11:46:53 -0300

    Inicio do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KwNautilus::Application.config.secret_token = '74e85c2ad8175724d2704b244f873d701fc36825cac2f2fe173021beda5404fd27c33d5f54b819e8d5c0fea4143a0c72e3ef5517c0ca04fab179190035de0d10'
