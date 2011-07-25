0b46f632b899f080d675dd3038fde2bc38109d88
Rafael Matuoka <rmatuoka@agenciaeconomica.com.br>
2011-07-25 15:18:00 -0300

    Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 KwNautilus::Application.routes.draw do
+  
+  
+  
+  
+  get "home/index"
+
+  root :to => "home#index"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
