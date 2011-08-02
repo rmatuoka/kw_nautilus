8723a58db8c60fbd1f6ab213d37b74a135b7bcd7
Bruno Silva <bsilva@korewa.com.br>
2011-08-02 16:56:36 -0300

    Videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,11 @@
 class VideosController < ApplicationController
   def index
+    @Videos = Video.all(:conditions => ['published = 1']).paginate(:page => params[:page], :per_page => 3)
   end
 
   def show
+    @Video = Video.find(params[:id])
+    @Link = @Video.link.sub("http://www.youtube.com/watch?v=","http://www.youtube.com/v/")
   end
 
 end
