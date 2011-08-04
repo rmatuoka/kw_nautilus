1558f6caf474552bf727a6c4e475e452aa660e68
Bruno Silva <bsilva@korewa.com.br>
2011-08-04 12:15:03 -0300

    Ajuste Novas Imagens

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -1,5 +1,5 @@
 class Highlight < ActiveRecord::Base
-    has_attached_file :image, :styles => { :thumb => "125x125>" },
+    has_attached_file :image, :styles => { :thumb => "125x125>", :destaque=>"950x315"},
         :url => "/uploads/:attachment/:id/:basename_:style.:extension",
         :path => ":rails_root/public/uploads/:attachment/:id/:basename_:style.:extension"
         
