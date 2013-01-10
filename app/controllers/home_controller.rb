f31fa7371bed8f569d7eec4ef3352d26cd1e2e38
Rafael Matuoka Chiocchetti <rmatuoka@gmail.com>
2013-01-10 16:52:45 -0200

    ajuste na home e nas revendas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,16 @@
 class HomeController < ApplicationController
+  before_filter :load_states
   
   layout "home" 
   
   def index
     @Depoimento = Testimonial.all(:conditions => ['published = 1'], :limit => 1, :order => 'created_at DESC')
     @Destaques = Highlight.all(:conditions => ['published = 1'], :order => "position ASC")    
-    @NoticiasHome = Notice.all(:conditions => ['published = 1'], :limit => 4, :order => "RAND()")
+    @NoticiasHome = Notice.all(:conditions => ['published = 1'], :limit => 4, :order => "id DESC")
   end
 
+  def load_states
+    @Estados = Reseller.all(:conditions => ['published = 1'], :group => "state", :order => "state ASC")
+  end
 end
 
